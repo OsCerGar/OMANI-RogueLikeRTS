14bbdb0b15dc3df70b1bfe15b0d7a1f26dee818c
CrisMarCab <crisw8mc@gmail.com>
2018-02-01 05:44:14 +0100

    Follow

---
+++ OMANI-v2.0/Assets/Scripts/NPC.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [SerializeField]
+    string BoyType = "Swordsman";
+
+    [SerializeField]
+    string state = "Free";
+
+    [SerializeField]
+    int Life, Attack;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string GetBoyType() {
+        return BoyType;
+    }
+    public string GetState()
+    {
+        return state;
+    }
+
+    public void SetType(string state)
+    {
+        this.BoyType = BoyType;
+    }
+
+    public void SetState(string state)
+    {
+        this.state = state;
+    }
+}
