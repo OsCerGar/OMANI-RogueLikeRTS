a75437131b659a425467324ec48d959d22b821b0
OsCerGar <oscergar@gmail.com>
2018-02-01 03:13:54 +0100

    Bboys Soldados V1 / Creep v1
    Primeras AIs funcionales en principio

---
+++ OMANI-v2.0/Assets/Scripts/TaskOrbitate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class TaskOrbitate : Action {
+
+    public SharedVector3 RightVector;
+    float speed = 0.1f;
+    // Use this for initialization
+    void Start () {
+
+	}
+
+    public override TaskStatus OnUpdate()
+    {
+
+        RightVector.SetValue(transform.right*speed);
+        return TaskStatus.Success;
+    }
+}
