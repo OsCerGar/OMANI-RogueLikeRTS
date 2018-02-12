aec5751257077fb2906005628c7e3fa509482f99
OsCerGar <oscergar@gmail.com>
2018-02-12 15:28:23 +0100

    BigJumpingCreep V1
    Primera versión de un nuevo enemigo.
    
    TODO:
    Estado Stun, en People.

---
+++ OMANI-v2.0/Assets/BE_Landing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public class BE_Landing : StateMachineBehaviour {
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        animator.transform.Find("AttackZone").gameObject.SetActive(true);
+        animator.SetBool("Landed", false);
+        var targetVariable = (SharedBool)animator.gameObject.GetComponent<BehaviorTree>().GetVariable("Jump");
+        targetVariable.Value = false;
+        animator.transform.Find("Splash").GetComponent<ParticleSystem>().Play();
+    }
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        
+
+    }
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ OMANI-v2.0/Assets/Be_StartJump.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class Be_StartJump : StateMachineBehaviour {
+    public GameObject efect;
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+        //start the jumping in tree behaviour (seek)
+        var targetVariable = (SharedBool)animator.gameObject.GetComponent<BehaviorTree>().GetVariable("Jump");
+        targetVariable.Value = true;
+
+        var position = (SharedVector3)animator.gameObject.GetComponent<BehaviorTree>().GetVariable("JumpObjective");
+        position.Value = (animator.transform.Find("JumpPosition").transform.position);
+
+      
+        
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
