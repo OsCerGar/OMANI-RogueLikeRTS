be968e3a8e883b991101b21c32dbc93e68d621bc
OsCerGar <oscergar@gmail.com>
2018-02-19 15:18:26 +0100

    BugFixing + NewAnim
    Fixed not wanted extra attacks (Archer) (Trigger Queue).
    New Warning Animation For jumping Creep
    Now The jump actually makes the creep go up.

---
+++ OMANI-v2.0/Assets/JumpControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class JumpControl : MonoBehaviour {
+    NavMeshAgent agent;
+    Animator anim;
+    // Update is called once per frame
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update () {
+        agent.baseOffset = anim.GetFloat("Height");
+	}
+}
