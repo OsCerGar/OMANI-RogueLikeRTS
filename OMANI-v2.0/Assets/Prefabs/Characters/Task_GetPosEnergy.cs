535be3391d50d6b02d0851c97cafa5a881eb072c
OsCerGar <oscergar@gmail.com>
2018-03-26 05:20:38 +0200

    WORKER positions for Powerplant

---
+++ OMANI-v2.0/Assets/Prefabs/Characters/Task_GetPosEnergy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tasks;
+using BehaviorDesigner.Runtime;
+
+public class Task_GetPosEnergy : Action
+{
+
+
+    // Update is called once per frame
+    public override TaskStatus OnUpdate()
+    {
+        //Creates a Sphere around himself and makes the target of every Ally the same as his
+        var thisTarget = (SharedGameObject)transform.gameObject.GetComponent<BehaviorTree>().GetVariable("Target");
+
+
+        if (thisTarget.Value.GetComponent<BU_PowerPlant>())
+        {
+            var PositiontoGo = thisTarget.Value.GetComponent<BU_PowerPlant>().givePosition();
+            thisTarget.Value = PositiontoGo.gameObject;
+            return TaskStatus.Success;
+        }
+        else
+        {
+            return TaskStatus.Failure;
+        }
+    }
+}
