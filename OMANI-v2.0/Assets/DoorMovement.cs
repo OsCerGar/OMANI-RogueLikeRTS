39e96987aad67ec0bf1886314b7354595a072a66
OsCerGar <oscergar@gmail.com>
2018-03-26 03:04:38 +0200

    Walls && Doors (cuts navmesh ;))

---
+++ OMANI-v2.0/Assets/DoorMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMovement : MonoBehaviour {
+    private Vector3 initialPos, openedPos;
+	// Use this for initialization
+	void Start () {
+        initialPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        openedPos = new Vector3(transform.Find("Opened").position.x, transform.Find("Opened").position.y, transform.Find("Opened").position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "People")
+        {
+            transform.position = Vector3.Lerp(openedPos, initialPos, Time.deltaTime);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "People")
+        {
+            transform.position = Vector3.Lerp(initialPos, openedPos, Time.deltaTime);
+        }
+    }
+}
