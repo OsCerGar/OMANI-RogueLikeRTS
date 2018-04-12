8cbf4792c61e011caff92db7a5720d150723f3e5
OsCerGar <oscergar@gmail.com>
2018-04-12 22:20:29 +0200

    MapGeneratorV2. Started

--- OMANI-v2.0/Assets/MapManager.cs
+++ OMANI-v2.0/Assets/MapManager.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MapManager : MonoBehaviour {
-    public int numberOfCamps;
-    public GameObject[] ResourcePrefab;
+
+    [SerializeField] float numberOfBigFeatures;
+    public GameObject[] Hills;
+
+
+    private  List<WorldFeature> BigFeatures;
+    [SerializeField]
+    GameObject BasePrefab;
 
     [SerializeField]
     GameObject CreepPrefab;
+
+
+
+    public int numberOfCamps;
+    public GameObject[] ResourcePrefab;
 
     [SerializeField]
     GameObject WorkerPrefab;
@@ -22,8 +33,29 @@ public class MapManager : MonoBehaviour {
     public Transform[] ArtifactPositions;
     public List<GameObject> Res = new List<GameObject>();
     List<int> usedNumbers = new List<int>();
+
+    //TerrainData
+    Terrain terrain;
+    private float tWidth,t;
+    private float tLength;
+
     // Use this for initialization
     void Start () {
+        //Getting terrian Data 
+        /*
+        BigFeatures = new List<WorldFeature>();
+        terrain = FindObjectOfType<Terrain>();
+        tWidth = terrain.terrainData.size.x;
+        tLength = terrain.terrainData.size.z;
+        
+        SpawnBase();
+
+        Debug.Log(BigFeatures.Count);
+
+        SpawnHills();
+        */
+        
+
         usedNumbers.Add(100000);
         FillResources();
         ActivateSavageCamp();
@@ -32,6 +64,53 @@ public class MapManager : MonoBehaviour {
         SpawnCreeps();
         SpawnWorkers();
         SpawnArtifacts();
+        
+    }
+
+    private void SpawnHills()
+    {
+        for (int i = 0; i < numberOfBigFeatures; i++)
+        {
+            var hillToSpawn = Hills[UnityEngine.Random.Range(0, Hills.Length)];
+            bool SpotFound = false;
+            while (!SpotFound)
+            {
+                //get a randomSpot in the terrain and 
+                Vector3 PosToSpawn = GetPosToSpawn();
+                if (checkDistances(PosToSpawn, hillToSpawn.GetComponent<WorldFeature>()))
+                {
+                    var newHill = Instantiate(hillToSpawn, PosToSpawn, Quaternion.Euler(0,UnityEngine.Random.Range(0, 180), 0));
+                    BigFeatures.Add(newHill.GetComponent<WorldFeature>());
+                    SpotFound = true;
+                }
+            }
+
+        }
+    }
+
+    private bool checkDistances(Vector3 _PosToSpawn, WorldFeature ThingToSpawn)
+    {
+        //Checks that the distance beetwen the thing u want to spawn and the rest of existing features is not too low 
+        for (int i = 0; i < BigFeatures.Count; i++)
+        {
+            if (Vector3.Distance(_PosToSpawn, BigFeatures[i].transform.position) < ThingToSpawn.Rad + BigFeatures[i].Rad)
+            {
+                return false;
+            }
+        }
+       
+        return true;
+    }
+
+    private Vector3 GetPosToSpawn()
+    {
+        return new Vector3(UnityEngine.Random.Range(-tWidth / 2, tWidth / 2), 0, UnityEngine.Random.Range(-tLength / 2, tLength / 2));
+    }
+
+    private void SpawnBase()
+    {
+        var mainBase  = (GameObject) Instantiate(BasePrefab, new Vector3(0, 0 , 0), BasePrefab.transform.rotation);
+        BigFeatures.Add( (WorldFeature )mainBase.GetComponent<WorldFeature>());
     }
 
     private void SpawnArtifacts()
---
+++ OMANI-v2.0/Assets/WorldFeature.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldFeature : MonoBehaviour {
+
+    public float Rad;
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, Rad);
+    }
+    public Vector3 getPos ()
+    {
+        return transform.position;
+        }
+
+}
