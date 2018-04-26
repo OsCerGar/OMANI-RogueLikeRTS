1ea7a10d585a3857ecd0aee01a46ee3a6d2c784a
OsCerGar <oscergar@gmail.com>
2018-04-26 14:46:32 +0200

    Bug Fixes
    -Tree Spawning ahora no spawnean en la base, y se organizan un poco mejor.
    -El navmesh de la base Salvaje Ya está bien.
    -Y de la base del prota tambien.
    -alguna cosa de behaviours tambien arreglada

--- OMANI-v2.0/Assets/MapManager.cs
+++ OMANI-v2.0/Assets/MapManager.cs
@@ -62,7 +62,6 @@ public class MapManager : MonoBehaviour {
         SpawnTrees();
 
         SpawnCreepPositions();
-        Debug.Log("hey");
 
         buildNavmesh();
         /*
@@ -120,43 +119,68 @@ public class MapManager : MonoBehaviour {
         {
             var hillToSpawn = Trees[UnityEngine.Random.Range(0, Trees.Length)];
             bool SpotFound = false;
-            
-                //get a randomSpot in the terrain and 
-                Vector3 PosToSpawn = GetPosToSpawn();
-                PosToSpawn = FindCloseTrees(PosToSpawn);
-           
-                RaycastHit hit;
-                if (Physics.Raycast(new Vector3(PosToSpawn.x, 20, PosToSpawn.z), Vector3.down, out hit, Mathf.Infinity))
-                {
-                    var newHill = Instantiate(hillToSpawn, hit.point, Quaternion.Euler(0, UnityEngine.Random.Range(0, 180), 0));
+        
+            //get a randomSpot in the terrain and 
+            Vector3 PosToSpawn = GetPosToSpawn();
+            //Make sure it's not in base :D
+            float baseRad = BasePrefab.GetComponent<WorldFeature>().Rad;
 
+            if (PosToSpawn.x < baseRad && PosToSpawn.x > -baseRad) //For X Pos
+            {
+                if (PosToSpawn.x > 0)
+                {
+                    PosToSpawn.x += baseRad;
                 }
+                else
+                {
+                    PosToSpawn.x -= baseRad;
+                }
+            }
+            if (PosToSpawn.z < baseRad && PosToSpawn.z > -baseRad) //For Z Pos
+            {
+                if (PosToSpawn.z > 0)
+                {
+                    PosToSpawn.z += baseRad;
+                }
+                else
+                {
+                    PosToSpawn.z -= baseRad;
+                }
+            }
+            PosToSpawn = FindCloseTrees(PosToSpawn);
+            RaycastHit hit;
+           
+            if (Physics.Raycast(new Vector3(PosToSpawn.x, 20, PosToSpawn.z), Vector3.down, out hit, Mathf.Infinity))
+            {
+                var newHill = Instantiate(hillToSpawn, hit.point, Quaternion.Euler(0, UnityEngine.Random.Range(0, 180), 0));
+
+            }
         }
     }
 
     private Vector3 FindCloseTrees(Vector3 _posToSpawn)
     {
-        float closeRange = 50;
-        int xOffset = 2, zOffset = 2;
-        Collider[] hitColliders = Physics.OverlapSphere(_posToSpawn, closeRange);
+        float closeRange = 30;
+        int xOffset = (int)UnityEngine.Random.Range(-2, 2), zOffset = (int)UnityEngine.Random.Range(-2, 2);
+        Collider[] hitColliders = Physics.OverlapSphere(_posToSpawn, closeRange); //List of close Trees (colliders)
         int i = 0;
-        while (i < hitColliders.Length)
+        while (i < hitColliders.Length) //Check all colliders
         {
-            WorldSmallFeature SF = hitColliders[i].GetComponent<WorldSmallFeature>();
-            if (SF != null)
+            WorldSmallFeature SF = hitColliders[i].GetComponent<WorldSmallFeature>(); 
+            if (SF != null) //If it's a WorldSmall Feature
             {
-                if (Vector3.Distance(_posToSpawn, hitColliders[i].transform.position) < closeRange)
+                if (Vector3.Distance(_posToSpawn, hitColliders[i].transform.position) < closeRange) //If close enough to it
                 {
                     closeRange = Vector3.Distance(_posToSpawn, hitColliders[i].transform.position);
+                    //Get a random position around the close feature
                     _posToSpawn = new Vector3(hitColliders[i].transform.position.x + xOffset, hitColliders[i].transform.position.y, hitColliders[i].transform.position.z + zOffset);
                     int SafetyNet = 0;
-                    while (!IsPosFree(_posToSpawn))
+                    while (!IsPosFree(_posToSpawn)) //If this position isn's free, change the offset to a bigger one
                     {
-                        xOffset = xOffset + (int)UnityEngine.Random.Range(-2,2);
-                        zOffset = zOffset + (int)UnityEngine.Random.Range(-2, 2);
-                        Debug.Log("changed pos");
+                        xOffset +=  (int)UnityEngine.Random.Range(-1,1);
+                        zOffset +=  (int)UnityEngine.Random.Range(-1, 1);
                         SafetyNet++;
-                        if (SafetyNet>10)
+                        if (SafetyNet>20)
                         {
                             break;
                         }
@@ -171,15 +195,14 @@ public class MapManager : MonoBehaviour {
 
     private bool IsPosFree(Vector3 _posToSpawn)
     {
-        Collider[] hitColliders = Physics.OverlapSphere(_posToSpawn, 3);
+        Collider[] hitColliders = Physics.OverlapSphere(_posToSpawn, 2);
         int i = 0;
         while (i < hitColliders.Length)
         {
             WorldFeature WF = hitColliders[i].GetComponent<WorldFeature>();
             if (WF != null)
             {
-
-                Debug.Log(WF.transform.name);
+                
                 return false;
 
             }
