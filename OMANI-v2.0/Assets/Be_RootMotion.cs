f6d8fd7398891dd47dc5c8468659d9635090cf67
OsCerGar <oscergar@gmail.com>
2018-06-07 16:55:33 +0200

    Animación de hit ahora es mas limpio. Menos room a problemas

---
+++ OMANI-v2.0/Assets/Be_RootMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using BehaviorDesigner.Runtime;
+
+public class Be_RootMotion : StateMachineBehaviour {
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.applyRootMotion = true;
+        animator.GetComponent<NavMeshAgent>().enabled = false;
+        animator.GetComponent<BehaviorTree>().enabled = false;
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.applyRootMotion = false;
+        animator.GetComponent<NavMeshAgent>().enabled = true;
+        animator.GetComponent<BehaviorTree>().enabled = true;
+    }
+    
+}
