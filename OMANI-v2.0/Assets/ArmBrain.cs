ab3024671719ae3c5376a01edcb2d2b6f64d614c
OsCerGar <oscergar@gmail.com>
2018-06-14 12:55:57 +0200

    Primera Version de las patitas

---
+++ OMANI-v2.0/Assets/ArmBrain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmBrain : MonoBehaviour {
+
+    [SerializeField] Transform ArmTarget;
+    public Vector3 TargetPos;
+    private Vector3 startPos;
+    private bool moving;
+    float x = 0;
+    private void Start()
+    {
+        TargetPos = ArmTarget.position;
+        moving = false;
+    }
+    private void Update()
+    {
+        if (moving)
+        {
+            x += (Time.deltaTime * 2f);
+            ArmTarget.position = MathParabola.Parabola(startPos, TargetPos, TargetPos.y+1f,  x);
+            if (x>0.95)
+            {
+                x = 0;
+                moving = false;
+
+            }
+        }
+    }
+    public void SetTargetPos(Vector3 _targetPos)
+    {
+        if (Vector3.Distance(_targetPos,TargetPos) > 1)
+        {
+            startPos = new Vector3(ArmTarget.position.x, ArmTarget.position.y, ArmTarget.position.z);
+               TargetPos = _targetPos;
+            moving = true;
+            x = 0;
+        }
+    }
+}
---
+++ OMANI-v2.0/Assets/Scripts/LocomotionBrain.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionBrain : MonoBehaviour {
+        private float Loop = 0;
+        private int footControler = 0;
+
+        public bool ikActive = false;
+
+        
+        public Transform lookObj = null;
+
+        public Transform rightHandPos = null;
+        public Transform leftHandPos = null;
+        public Transform rightFootPos = null;
+        public Transform leftFootPos = null;
+
+    public GameObject ArmPositions;
+
+    private Vector3 IKrightHandPos;
+    private Vector3 IKleftHandPos;
+    private Vector3 IKrightFootPos;
+    private Vector3 IKleftFootPos;
+
+    private ArmBrain RightHandBrain;
+    private ArmBrain LeftHandBrain;
+    private ArmBrain RightFootBrain;
+    private ArmBrain LeftFootBrain;
+    Rigidbody playerRB;
+
+    void Start()
+    {
+        playerRB = transform.parent.GetComponent<Rigidbody>();
+        RightHandBrain = rightHandPos.GetComponent<ArmBrain>(); 
+        LeftHandBrain = leftHandPos.GetComponent<ArmBrain>();
+        RightFootBrain = rightFootPos.GetComponent<ArmBrain>();
+        LeftFootBrain = leftFootPos.GetComponent<ArmBrain>();
+        IKrightHandPos = new Vector3(rightHandPos.position.x, rightHandPos.position.y, rightHandPos.position.z);
+        IKleftHandPos = new Vector3(leftHandPos.position.x, leftHandPos.position.y, leftHandPos.position.z) ;
+        IKrightFootPos = new Vector3(rightFootPos.position.x, rightFootPos.position.y, rightFootPos.position.z);
+        IKleftFootPos = new Vector3(leftFootPos.position.x,leftFootPos.position.y, leftFootPos.position.z) ;
+    }
+
+    private void Update()
+    {
+        Loop += Time.deltaTime;
+        if (Loop >= 0.2 - (playerRB.velocity.magnitude / 100) )
+        {
+            switch (footControler)
+            {
+                case 0:
+                    IKrightHandPos = ShootRaycast(rightHandPos);
+                    break;
+                case 1:
+                    IKleftHandPos = ShootRaycast(leftHandPos);
+                    break;
+                case 2:
+                    IKrightFootPos = ShootRaycast(rightFootPos);
+                    break;
+                case 3:
+                    IKleftFootPos = ShootRaycast(leftFootPos);
+                    break;
+
+                default:
+                    print("Incorrect ");
+                    break;
+            }
+            
+            
+            
+            footControler++;
+            if (footControler == 4)
+            {
+                footControler = 0;
+            }
+            Loop = 0;
+        }
+        if (ikActive)
+        {
+            // Set the right hand target position and rotation, if one has been assigned
+            if (IKrightHandPos != null)
+            {
+                RightHandBrain.SetTargetPos(IKrightHandPos);
+            }
+            if (IKleftHandPos != null)
+            {
+                LeftHandBrain.SetTargetPos(IKleftHandPos);
+            }
+            if (IKrightFootPos != null)
+            {
+                RightFootBrain.SetTargetPos(IKrightFootPos);
+            }
+            if (IKleftFootPos != null)
+            {
+
+                LeftFootBrain.SetTargetPos(IKleftFootPos);
+            }
+
+
+        }
+
+        //if the IK is not active, set the position and rotation of the hand and head back to the original position
+        else
+        {
+            //TODO
+        }
+        ArmPositions.transform.position = transform.parent.position + playerRB.velocity / 2f;
+
+
+
+}
+
+Vector3 ShootRaycast(Transform tr)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(tr.position, -Vector3.up, out hit))
+        {
+            return hit.point;
+        }
+        return transform.position;
+    }
+
+   
+    
+}
