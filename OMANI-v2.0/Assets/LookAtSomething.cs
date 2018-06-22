0bc0188cabeb83519fe3deba02df82f2ce1e0c03
OsCerGar <oscergar@gmail.com>
2018-06-22 11:08:49 +0200

    NuevoScript Para lo de los tutoriales

---
+++ OMANI-v2.0/Assets/LookAtSomething.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtSomething : MonoBehaviour {
+
+    [SerializeField]Transform ThingToLook ;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        transform.LookAt(ThingToLook);
+    }
+}
---
+++ OMANI-v2.0/Assets/exPlicativoTreeControler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using UnityEngine.AI;
+using System;
+
+public class exPlicativoTreeControler : MonoBehaviour {
+         [SerializeField] GameObject[] holograms; 
+
+       public delegate void ActivateTutorial(Transform posToGo,string whatToShow);
+       public static event ActivateTutorial Tutorial;//un evento para cada fragmento del tutorial
+
+    [SerializeField] BehaviorTree TutorialBehaviour;
+    
+        void OnEnable()
+        {
+        Tutorial += ActivateMovementTut;
+        }
+
+
+        void OnDisable()
+        {
+        Tutorial -= ActivateMovementTut;
+        }
+
+
+        void ActivateMovementTut(Transform posToGo, string whatToShow)
+        {
+            ClearHolograms();
+        foreach (var hologram in holograms)
+        {
+            if (hologram.name == whatToShow)
+            {
+                //gets the Vars
+                TutorialBehaviour.enabled = true;
+                var posToGoVar = (SharedTransform)TutorialBehaviour.GetVariable("posToGo");
+                posToGoVar.Value = posToGo;
+                var hologramVar = (SharedGameObject)TutorialBehaviour.GetVariable("hologram");
+                hologramVar.Value = hologram;
+            }
+        }
+        }
+
+    private void ClearHolograms()
+    {
+        foreach (var hologram in holograms)
+        {
+            //gets the Vars
+            hologram.SetActive(false);
+            
+        }
+    }
+    
+}
