3815b476d05d92bab6f24d5d8145505c5c6b9bc4
CrisMarCab <crisw8mc@gmail.com>
2018-06-26 18:25:06 +0200

    Warv1

---
+++ OMANI-v2.0/Assets/TutorialWarEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialWarEvent : MonoBehaviour {
+    NPC[] npcArrays;
+    GameObject Player;
+	// Use this for initialization
+	void Start () {
+        npcArrays = GetComponentsInChildren<NPC>();
+        foreach (var item in npcArrays)
+        {
+            item.Damage = 0;
+            Player = GameObject.Find("Player");
+            
+        }
+    }
+	
+	void ActivateDeath()
+    {
+        foreach (var item in npcArrays)
+        {
+            item.Damage = 20;
+            if (item.GetType() ==  typeof(Creep))
+            {
+                item.AI_SetEnemy(Player);
+            }
+        }
+    }
+}
