d5f6f7d301e3615714d1acf867a562b7520348a6
CrisMarCab <crisw8mc@gmail.com>
2018-06-29 19:59:00 +0200

    MeshManager

---
+++ OMANI-v2.0/Assets/Scripts/Gameplay/NPC/MeshManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour {
+    List<Material> OriginalMats;
+    List<Renderer> Renderers;
+    private void Start()
+    {
+        var findMaterials = GetComponentsInChildren<Renderer>();
+
+        foreach (var item in findMaterials)
+        {
+            Renderers.Add(item);
+            OriginalMats.Add(new Material(item.material));
+        }
+
+    }
+    public void ReplaceMats (Material _mat)
+        {
+            foreach (var item in Renderers)
+            {
+                item.material = _mat;
+            }
+        }
+    public void Restore()
+    {
+        for (int i = 0; i < Renderers.Count; i++)
+        {
+            Renderers[i].material = OriginalMats[i];
+        }
+    }
+
+}
