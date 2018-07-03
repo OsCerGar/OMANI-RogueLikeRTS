6fce1c1eb96860ef3d6485ea1dd63c6f18707e1a
OsCerGar <oscergar@gmail.com>
2018-07-03 18:34:30 +0200

    Shitton fo particles boooyssss

---
+++ OMANI-v2.0/Assets/PowerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZObjectPools;
+
+public class PowerManager : MonoBehaviour
+{
+
+    EZObjectPool BasicPower, UpgradedPower;
+    GameObject Spawned;
+    [SerializeField] bool PressUtoSpawn;
+    Transform Player;
+    // Use this for initialization
+    void Start()
+    {
+        Player = GameObject.Find("Player").transform;
+        var AllPoolers = FindObjectsOfType<EZObjectPool>();
+        foreach (EZObjectPool item in AllPoolers)
+        {
+            if (item.PoolName == "BasicPower")
+            {
+                BasicPower = item;
+            }
+            if (item.PoolName == "UpgradedPower")
+            {
+                UpgradedPower = item;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (PressUtoSpawn)
+        {
+
+            if (Input.GetKeyDown(KeyCode.U))
+            {
+                ShootBasicPower(Player);
+            }
+        }
+    }
+
+    public void ShootBasicPower(Transform tr)
+    {
+        BasicPower.TryGetNextObject(tr.position, tr.rotation, out Spawned);
+    }
+    public void ShootUpgradedPower(Transform tr)
+    {
+        UpgradedPower.TryGetNextObject(tr.position, tr.rotation, out Spawned);
+    }
+}
+
---
+++ OMANI-v2.0/Assets/Script/PowerDebugger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerDebugger : MonoBehaviour {
+
+    public PowerManager PM;
+    public Transform CannonMouth;
+
+    private void Start()
+    {
+        PM = FindObjectOfType<PowerManager>();
+    }
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            PM.ShootBasicPower(CannonMouth);
+        }
+        
+	
+	}
+}
