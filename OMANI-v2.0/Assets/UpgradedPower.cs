7e0490f60edfba2fd5e08feb27094e626ded434c
OsCerGar <oscergar@gmail.com>
2018-07-05 15:49:09 +0200

    upgraded power not working

---
+++ OMANI-v2.0/Assets/UpgradedPower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradedPower : MonoBehaviour {
+    [SerializeField]ParticleSystem PS;
+    private void OnTriggerEnter(Collider other)
+    {
+            Enemy NPC;
+            if (NPC = other.GetComponent<Enemy>())
+            {
+                PS.transform.position = other.transform.position;
+                PS.Play();
+                NPC.TakeDamage(25);
+            }
+    }
+}
