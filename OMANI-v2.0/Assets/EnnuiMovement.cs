6307c29172a381860e3b54fccbbf4a15a66c1623
OsCerGar <oscergar@gmail.com>
2018-07-29 15:52:40 +0200

    Ennui y algun cambio de combate y tal y demas cosas divertidas

---
+++ OMANI-v2.0/Assets/EnnuiMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnuiMovement : MonoBehaviour {
+    [SerializeField]float Force;
+    [SerializeField] Rigidbody[] AllRb;
+    float counter;
+
+    private void Update()
+    {
+        counter += Time.deltaTime;
+        if (counter > 1)
+        {
+
+        foreach (var item in AllRb)
+            {
+            var randomizer = new Vector3(Random.Range(-Force, Force), Random.Range(-Force, Force), Random.Range(-Force, Force));
+            item.AddForce(randomizer,ForceMode.Force);
+            }
+            counter = 0;
+        }
+    }
+}
--- OMANI-v2.0/Assets/Scripts/Enemy.cs
+++ OMANI-v2.0/Assets/Scripts/Enemy.cs
@@ -7,6 +7,5 @@ public class Enemy : NPC {
     public override void Die()
     {
         base.Die();
-        Nav.enabled = false;
     }
 }
