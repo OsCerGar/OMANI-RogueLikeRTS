fc3711bf4eb3208b91ccd6d4ce8ecc3cc042e1ce
CrisMarCab <crisw8mc@gmail.com>
2018-09-01 12:34:48 +0200

    AuraLightFixScript

---
+++ OMANI-v2.0/Assets/AuraLightsFix.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AuraAPI;
+
+[ExecuteInEditMode]
+public class AuraLightsFix : MonoBehaviour
+{
+
+    [SerializeField]
+    List<AuraLight> AuraLights;
+
+    private void Update()
+    {
+        if (!Application.isPlaying)
+        {
+            foreach (AuraLight light in AuraLights)
+            {
+                light.enabled = true;
+            }
+            Debug.Log("AuraLightFix in Update");
+        }
+    }
+}
