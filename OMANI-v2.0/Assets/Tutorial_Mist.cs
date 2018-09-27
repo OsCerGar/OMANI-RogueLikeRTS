8626866d0d6682961a2b9b72bf31cf4f6a6257a5
CrisMarCab <crisw8mc@gmail.com>
2018-09-27 12:58:47 +0200

    Ataque sorpresa final tutorial

--- OMANI-v2.0/Assets/Tutorial_Mist.cs
+++ OMANI-v2.0/Assets/Tutorial_Mist.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class Tutorial_Mist : MonoBehaviour
 {
-
+    [SerializeField]
+    GameObject toSpawn;
     Transform player;
     [SerializeField]
     AudioSource Horn;
+    bool attacked;
 
     public Transform other;
     public float closeDistance = 5.0f;
     private float timer, oldIntensity;
 
-    float minimumDistance = 5.0f;
-    float maximumDistance = 100.0f;
+    float minimumDistance = 500.0f;
+    float maximumDistance = 3000f;
 
     float minimumDistanceScaleFog;
     float minimumDistanceScaleFogStart;
-    float maximumDistanceScaleFog = 60.39f;
-    float maximumDistanceScaleFogStart = 54.6f;
+    float maximumDistanceScaleFog = 35f;
+    float maximumDistanceScaleFogStart = 15f;
     float sqrLen;
 
     // Use this for initialization
@@ -35,11 +37,12 @@ public class Tutorial_Mist : MonoBehaviour
     {
         Vector3 offset = player.position - transform.position;
         sqrLen = offset.sqrMagnitude;
-        if (sqrLen < closeDistance * closeDistance)
+        if (sqrLen < closeDistance * closeDistance && !attacked)
         {
             Attack();
         }
-        else if (sqrLen - (closeDistance * closeDistance) < 200f)
+
+        else if (sqrLen < 3000f)
         {
             Travelin();
         }
@@ -50,8 +53,7 @@ public class Tutorial_Mist : MonoBehaviour
     {
         float norm = (sqrLen - minimumDistance) / (maximumDistance - minimumDistance);
         norm = Mathf.Clamp01(norm);
-        Debug.Log("norm " + norm);
-        Debug.Log("fogstartdistnace " + Mathf.Lerp(maximumDistanceScaleFogStart, minimumDistanceScaleFogStart, norm));
+        Debug.Log(norm);
 
         RenderSettings.fogStartDistance = Mathf.Lerp(maximumDistanceScaleFogStart, minimumDistanceScaleFogStart, norm);
         RenderSettings.fogEndDistance = Mathf.Lerp(maximumDistanceScaleFog, minimumDistanceScaleFog, norm);
@@ -60,6 +62,8 @@ public class Tutorial_Mist : MonoBehaviour
 
     private void Attack()
     {
+        attacked = true;
         Horn.Play();
+        toSpawn.SetActive(true);
     }
 }
