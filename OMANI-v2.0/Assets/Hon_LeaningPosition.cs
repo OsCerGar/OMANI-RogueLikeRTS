cda7e207ceafc71f9a0babcce046f071c199f791
OsCerGar <oscergar@gmail.com>
2018-10-31 16:58:28 +0100

    Hon, animaciones, modelo, y algun que otro script

---
+++ OMANI-v2.0/Assets/Hon_LeaningPosition.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hon_LeaningPosition : MonoBehaviour {
+    Transform player, hon;
+    [SerializeField] float playerRadius, honRadius;
+    private IEnumerator coroutine;
+    // Use this for initialization
+    void Start () {
+
+        player = FindObjectOfType<Player>().transform;
+        hon = FindObjectOfType<Hon>().transform;
+
+        coroutine = WaitAndCheck(5.0f);
+        StartCoroutine(coroutine);
+
+    }
+
+    private IEnumerator WaitAndCheck(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            CheckDistances();
+        }
+    }
+
+    private void CheckDistances()
+    {
+        if (Vector3.Distance(transform.position, player.position) < playerRadius)
+        {
+
+            SetScreenColor(Color.yellow);
+            if (Vector3.Distance(transform.position, hon.position) < honRadius)
+            {
+
+                SetScreenColor(Color.red);
+                hon.GetComponent<Hon>().GoAttack(this.transform);
+            }
+        }
+        else
+        {
+            SetScreenColor(Color.green);
+        }
+    }
+    private void SetScreenColor(Color _color)
+    {
+        Renderer rend = GetComponentInChildren<Renderer>();
+        
+
+        //Set the main Color of the Material to green
+        rend.material.color = _color;
+       
+    }
+}
---
+++ OMANI-v2.0/Assets/Scripts/NPC/Hon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hon : Enemy
+
+{
+    bool interactingWithPlayer;
+    Vector3 _posToLean;
+    Quaternion _rotToLean;
+    public void GoAttack(Transform _pos)
+    {
+        
+        _posToLean = _pos.position;
+        _rotToLean = _pos.rotation;
+        interactingWithPlayer = true;
+        Nav.enabled = false;
+        enableTree("nothing");
+        anim.SetTrigger("Disappear");
+    }
+    public void Appear()
+    {
+        anim.SetTrigger("Appear");
+        WaitAndSwap();
+    }
+    override public  void Update()
+    {
+        //anim.MatchTarget(matchPosition.Value, matchRotation.Value, targetBodyPart, new MatchTargetWeightMask(weightMaskPosition, weightMaskRotation), startNormalizedTime, targetNormalizedTime);
+    }
+    private IEnumerator WaitAndSwap()
+    {
+            yield return new WaitForSeconds(1f);
+            transform.rotation = _rotToLean;
+            transform.position = _posToLean;
+    }
+}
+
+	
