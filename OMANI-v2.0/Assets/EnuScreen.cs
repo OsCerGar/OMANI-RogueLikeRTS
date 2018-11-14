d6e44374af5262513f5f8cef6b7e2db9ce3e54ba
CrisMarCab <crisw8mc@gmail.com>
2018-11-14 16:47:57 +0100

    EnuScreens

---
+++ OMANI-v2.0/Assets/EnuScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnuScreen : MonoBehaviour
+{
+
+    Animator animator;
+    BU_Energy_CityDistricts EnergyDistrict;
+    float animationValue;
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        EnergyDistrict = this.transform.parent.GetComponentInParent<BU_Energy_CityDistricts>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animationValue = Mathf.Lerp(animationValue, 0.02f * EnergyDistrict.totalEnergyReturn(), 0.75f * Time.deltaTime);
+
+        animator.Play("EnuScreen", 0, Mathf.Clamp(animationValue, 0, 1));
+
+    }
+}
