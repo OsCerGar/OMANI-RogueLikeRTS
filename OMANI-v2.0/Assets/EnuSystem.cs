bdfa33aec597e2531d056bf088a43a744053495e
CrisMarCab <crisw8mc@gmail.com>
2018-11-28 19:27:49 +0100

    EnuSystem animations + speed

--- OMANI-v2.0/Assets/EnuSystem.cs
+++ OMANI-v2.0/Assets/EnuSystem.cs
@@ -10,11 +10,31 @@ public class EnuSystem : MonoBehaviour
     void Start()
     {
         wackaEnus.AddRange(GetComponentsInChildren<WackaEnu>());
+        systemAnimator = GetComponent<Animator>();
+        systemAnimator.SetInteger("State", Random.Range(1, 6));
+
     }
 
     public void resetSystem()
     {
         foreach (WackaEnu wackaEnu in wackaEnus) { wackaEnu.resetEnu(); }
+        systemAnimator.SetFloat("Speed", systemAnimator.GetFloat("Speed") + 0.1f);
+
+        bool done = false;
+        int randomRange = Random.Range(1, 6);
+
+        while (!done)
+        {
+            if (randomRange == systemAnimator.GetFloat("State"))
+            {
+                randomRange = Random.Range(1, 6);
+            }
+            else
+            {
+                systemAnimator.SetInteger("State", Random.Range(1, 6));
+                done = true;
+            }
+        }
     }
 
 }
