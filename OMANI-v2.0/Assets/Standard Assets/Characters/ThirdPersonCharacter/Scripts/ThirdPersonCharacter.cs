f2a6ba69cd9f4e66a1cfa2148d13436071831d98
OsCerGar <oscergar@gmail.com>
2019-01-08 14:23:49 +0100

    ultimos tweaks al movimiento del robot

--- OMANI-v2.0/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
+++ OMANI-v2.0/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonCharacter.cs
@@ -6,40 +6,27 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 	[RequireComponent(typeof(CapsuleCollider))]
 	[RequireComponent(typeof(Animator))]
 	public class ThirdPersonCharacter : MonoBehaviour
-	{
-		[SerializeField] float m_MovingTurnSpeed = 360;
+    {
+        [SerializeField] bool Rotate = false;
+        [SerializeField] float m_MovingTurnSpeed = 360;
 		[SerializeField] float m_StationaryTurnSpeed = 180;
-		[SerializeField] float m_JumpPower = 12f;
-		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
-		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
 		[SerializeField] float m_MoveSpeedMultiplier = 1f;
 		[SerializeField] float m_AnimSpeedMultiplier = 1f;
-		[SerializeField] float m_GroundCheckDistance = 0.1f;
-
+        Vector3 move;
 		Rigidbody m_Rigidbody;
 		Animator m_Animator;
 		bool m_IsGrounded;
 		float m_OrigGroundCheckDistance;
-		const float k_Half = 0.5f;
 		float m_TurnAmount;
 		float m_ForwardAmount;
-		Vector3 m_GroundNormal;
-		float m_CapsuleHeight;
-		Vector3 m_CapsuleCenter;
-		CapsuleCollider m_Capsule;
-		bool m_Crouching;
 
 
 		void Start()
 		{
 			m_Animator = GetComponent<Animator>();
 			m_Rigidbody = GetComponent<Rigidbody>();
-			m_Capsule = GetComponent<CapsuleCollider>();
-			m_CapsuleHeight = m_Capsule.height;
-			m_CapsuleCenter = m_Capsule.center;
 
 			m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
-			m_OrigGroundCheckDistance = m_GroundCheckDistance;
 		}
 
 
@@ -48,19 +35,21 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
             // convert the world relative moveInput vector into a local-relative
             // turn amount and forward amount required to head in the desired
-            // direction.//ApplyExtraTurnRotation();
+            // direction.//
             if (move.magnitude > 1f) move.Normalize();
 			move = transform.InverseTransformDirection(move);
-			move = Vector3.ProjectOnPlane(move, m_GroundNormal);
-			//m_TurnAmount = Mathf.Atan2(move.x, move.z);
             m_TurnAmount = move.x;
             m_ForwardAmount = move.z;
+            if (Rotate)
+            {
+                ApplyExtraTurnRotation();
+            }
 
-			
-            
-			
-            
-			UpdateAnimator(move);
+
+
+
+
+            UpdateAnimator(move);
 		}
         
 
@@ -84,8 +73,9 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 
 		void ApplyExtraTurnRotation()
 		{
-			// help the character turn faster (this is in addition to root rotation in the animation)
-			float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+            // help the character turn faster (this is in addition to root rotation in the animation)
+            m_TurnAmount = Mathf.Atan2(move.x, move.z);
+            float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
 			transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
 		}
 
