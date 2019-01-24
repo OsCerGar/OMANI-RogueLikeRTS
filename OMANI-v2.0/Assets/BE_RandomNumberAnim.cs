e4b0fae2ca831fdcd1932a191ffd119a5f6b326e
CrisMarCab <crisw8mc@gmail.com>
2019-01-24 19:56:03 +0100

    Revert "Revert "retoke""
    This reverts commit 7dbbdd8b7da803fde4adccd9283520ec1bc5e40e.

--- OMANI-v2.0/Assets/BE_RandomNumberAnim.cs
+++ OMANI-v2.0/Assets/BE_RandomNumberAnim.cs
@@ -5,7 +5,7 @@ public class BE_RandomNumberAnim : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        animator.SetInteger("NumberIndex", Random.Range(0, 2));
+        animator.SetInteger("NumberIndex", Random.Range(0, 3));
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
