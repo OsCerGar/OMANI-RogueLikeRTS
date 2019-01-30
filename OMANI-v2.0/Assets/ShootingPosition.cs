98bb48a3043509d5bd659ec6bc09b75d1ab642fc
OsCerGar <oscergar@gmail.com>
2019-01-30 15:50:20 +0100

    Mejorado como orbitan los robots a la reina

--- OMANI-v2.0/Assets/ShootingPosition.cs
+++ OMANI-v2.0/Assets/ShootingPosition.cs
@@ -6,14 +6,54 @@ public class ShootingPosition : MonoBehaviour {
     LookDirectionsAndOrder LDAO;
     Transform parent;
     [SerializeField]float rad;
-	// Use this for initialization
-	void Start () {
+    [SerializeField] float speed;
+    // Use this for initialization
+    void Start () {
         LDAO = FindObjectOfType<LookDirectionsAndOrder>();
         parent = transform.parent;
+
+
+        transform.position = parent.position + new Vector3(rad,0,0);
+        
     }
 	
 	// Update is called once per frame
-	void LateUpdate () {
-        transform.position = (parent.position + ((LDAO.miradaposition - parent.position).normalized * rad));
-	}
+	void Update () {
+        var newmiradapos = new Vector3(LDAO.miradaposition.x, parent.position.y, LDAO.miradaposition.z);
+        var posToGo = (parent.position + ((newmiradapos - parent.position).normalized * rad));
+
+        transform.LookAt(parent.position);
+
+        if (transform.InverseTransformPoint(posToGo).x > 0)
+        {
+            transform.RotateAround(parent.position, Vector3.up,-Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.RotateAround(parent.position, Vector3.up, Time.deltaTime * speed);
+        }
+       
+
+
+        }
+
+    float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up)
+    {
+        Vector3 perp = Vector3.Cross(fwd, targetDir);
+        float dir = Vector3.Dot(perp, up);
+
+        if (dir > 0f)
+        {
+            return 1f;
+        }
+        else if (dir < 0f)
+        {
+            return -1f;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+
 }
