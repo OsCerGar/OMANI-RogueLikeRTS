dd10caa04178a5da49a303b381a65cd77f26ae3b
CrisMarCab <crisw8mc@gmail.com>
2019-02-05 21:20:49 +0100

    update

---
+++ OMANI-v2.0/Assets/Tutorial_PlayerLock.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Tutorial_PlayerLock : MonoBehaviour
+{
+    public OMANINPUT controls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls.PLAYER.WASD.Disable();
+        controls.PLAYER.Joystick.Disable();
+
+    }
+}
