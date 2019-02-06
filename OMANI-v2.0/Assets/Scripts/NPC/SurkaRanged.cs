51aff327f13ad27de7cd0cc891f367ac9b88aa54
OsCerGar <oscergar@gmail.com>
2019-02-06 19:51:17 +0100

    SurkaFrog

--- OMANI-v2.0/Assets/Scripts/NPC/SurkaRanged.cs
+++ OMANI-v2.0/Assets/Scripts/NPC/SurkaRanged.cs
@@ -3,36 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using BehaviorDesigner.Runtime.Tasks;
 using BehaviorDesigner.Runtime;
-using EZObjectPools;
 
 public class SurkaRanged : Enemy {
-    [SerializeField]GameObject weapon;
-    GameObject throwspear;
-    private void Awake()
-    {
-        throwspear = GetComponentInChildren<RangedSpear>().gameObject;
-    }
+    [SerializeField] ParticleSystem AttackTrail;
+    [SerializeField] ParticleSystem Shoot;
+
     public void SetMaster(GameObject master)
     {
         var thisTarget = (SharedGameObject)transform.gameObject.GetComponent<BehaviorTree>().GetVariable("Master");
         thisTarget.Value = master;
 
     }
-   
+    public void StartAttackTrail()
+        {
+             AttackTrail.Play();
+        }
+
     public override void AttackHit()
     {
-
-        weapon.SetActive(false);
-        throwspear.SetActive(true);
-        throwspear.transform.position = transform.position;
-        if (AI_GetEnemy() != null)
-        {
-            throwspear.GetComponent<RangedSpear>().setDestination(weapon.transform, AI_GetEnemy().transform.position + transform.forward * 2);
-        }
-        else
-        {
-            throwspear.GetComponent<RangedSpear>().setDestination(weapon.transform, this.transform.position + transform.forward * 10);
-        }
+        Shoot.Emit(1);
     }
 
 }
