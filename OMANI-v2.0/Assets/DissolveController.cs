fd224a186ee2b7ddb22563bcb5f7e4082b35b25b
CrisMarCab <crisw8mc@gmail.com>
2019-02-08 12:23:27 +0100

    Dissolve

---
+++ OMANI-v2.0/Assets/DissolveController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+    [SerializeField] Material dissolvingMaterial;
+    bool dissolve = false;
+    float dissolveAmount = 0.05f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dissolve)
+        {
+            dissolveAmount += Time.deltaTime / 2;
+            MK.Toon.MKToonMaterialHelper.SetDissolveAmount(dissolvingMaterial, dissolveAmount);
+        }
+    }
+
+    private void OnEnable()
+    {
+        MK.Toon.MKToonMaterialHelper.SetDissolveAmount(dissolvingMaterial, 0);
+    }
+
+    private void OnDisable()
+    {
+        MK.Toon.MKToonMaterialHelper.SetDissolveAmount(dissolvingMaterial, 0);
+    }
+
+    public void StartDissolving() { dissolve = true; }
+}
--- OMANI-v2.0/Assets/Tutorial_ArmLock.cs
+++ OMANI-v2.0/Assets/Tutorial_ArmLock.cs
@@ -4,11 +4,18 @@ public class Tutorial_ArmLock : Enemy
 {
     [SerializeField] Tutorial_PlayerLock playerLock;
     [SerializeField] int legToRelease;
+    [SerializeField] DissolveController dissolveController;
 
     public override void Die()
     {
         playerLock.LegRelease(legToRelease);
         enabled = false;
+        dissolveController.StartDissolving();
+    }
+
+    public override void Update()
+    {
+        base.Update();
     }
 
 }
