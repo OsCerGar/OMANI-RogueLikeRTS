5b2fc4a287a2e12c1c6867939f5d22df7902a2cf
CrisMarCab <crisw8mc@gmail.com>
2019-02-11 18:12:11 +0100

    0 damage with laser to ArmLocks

--- OMANI-v2.0/Assets/Tutorial_ArmLock.cs
+++ OMANI-v2.0/Assets/Tutorial_ArmLock.cs
@@ -18,4 +18,40 @@ public class Tutorial_ArmLock : Enemy
         base.Update();
     }
 
+    //Simple way to take damage
+    public override void TakeDamage(int damage, Color damageType)
+    {
+        if (damageType == Color.green)
+        {
+            numberPool.NumberSpawn(numbersTransform, 0, damageType);
+        }
+
+        else
+        {
+            numberPool.NumberSpawn(numbersTransform, damage, damageType);
+
+            StartCoroutine(gotHit());
+            if (state == "Alive")
+            {
+                if (anim != null)
+                {
+                    anim.SetTrigger("Hit");
+                }
+                life -= damage;
+
+                if (life <= 0)
+                {
+                    Die();
+                    state = "Dead";
+                }
+            }
+        }
+    }
+
+    //Max damage is decided by the laser, in case of future upgrades to the Laser. 
+    public override void TakeWeakLaserDamage(float damage, int maxDamage)
+    {
+        TakeDamage(0, Color.green);
+    }
+
 }
