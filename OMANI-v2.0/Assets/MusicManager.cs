5d92f8d8fdedd43d647bc3b23c90dcb62f3f239a
CrisMarCab <crisw8mc@gmail.com>
2019-03-21 04:56:05 +0100

    Particles + music

--- OMANI-v2.0/Assets/MusicManager.cs
+++ OMANI-v2.0/Assets/MusicManager.cs
@@ -6,6 +6,11 @@ public class MusicManager : MonoBehaviour
     AudioSource combatMusic, combatStop, mainTheme;
     bool isPlaying;
     int numberOfEnemies = 0;
+    float timeToStop;
+
+    float lerpTime = 10f;
+    float currentLerpTime;
+
 
     void OnEnable()
     {
@@ -23,7 +28,7 @@ public class MusicManager : MonoBehaviour
     void enemyDied(Enemy died)
     {
         numberOfEnemies--;
-
+        timeToStop = Time.time;
         StartCoroutine("stopMusic");
 
     }
@@ -32,10 +37,51 @@ public class MusicManager : MonoBehaviour
     {
         if (!isPlaying)
         {
+            currentLerpTime = 0;
             combatMusic.Play(); mainTheme.volume = 0;
             isPlaying = true;
         }
         numberOfEnemies++;
+    }
+
+    void Update()
+    {
+        //stops after 30 seconds
+        if (Time.time - timeToStop > 30f)
+        {
+            timeToStop = 999;
+            StopCombatMusic();
+        }
+
+        if (isPlaying)
+        {
+            currentLerpTime += Time.deltaTime;
+            float t = currentLerpTime / lerpTime;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            combatMusic.volume = Mathf.Lerp(combatMusic.volume, 0.2f, t);
+            mainTheme.volume = Mathf.Lerp(mainTheme.volume, 0f, t);
+        }
+        else
+        {
+            currentLerpTime += Time.deltaTime;
+            float t = currentLerpTime / lerpTime;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            combatMusic.volume = Mathf.Lerp(combatMusic.volume, 0f, t);
+            mainTheme.volume = Mathf.Lerp(mainTheme.volume, 0.85f, t);
+
+            if (combatMusic.isPlaying && combatMusic.volume <= 0.02f)
+            {
+                combatMusic.Stop();
+                combatStop.Play();
+                numberOfEnemies = 0;
+                currentLerpTime = 0f;
+            }
+        }
+    }
+
+    private void StopCombatMusic()
+    {
+        isPlaying = false;
     }
 
     // Start is called before the first frame update
@@ -51,11 +97,7 @@ public class MusicManager : MonoBehaviour
         if (numberOfEnemies == 0)
         {
             yield return new WaitForSeconds(4.5f);
-
-            combatMusic.Stop();
-            combatStop.Play();
-            mainTheme.volume = 0.85f;
-            isPlaying = false;
+            StopCombatMusic();
         }
     }
 
