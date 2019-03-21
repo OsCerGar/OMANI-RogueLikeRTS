ba8230661ebb8dce0202697e93b17927b498ba44
CrisMarCab <crisw8mc@gmail.com>
2019-03-21 13:54:45 +0100

    teleportes mas cuadrado + piernas reina

--- OMANI-v2.0/Assets/BE_Teleporter.cs
+++ OMANI-v2.0/Assets/BE_Teleporter.cs
@@ -3,19 +3,7 @@
 public class BE_Teleporter : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         SceneLoad scene = animator.gameObject.GetComponentInParent<SceneLoad>();
         if (scene != null)
@@ -24,6 +12,22 @@ public class BE_Teleporter : StateMachineBehaviour
         }
     }
 
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    /*override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SceneLoad scene = animator.gameObject.GetComponentInParent<SceneLoad>();
+        if (scene != null)
+        {
+            scene.LoadNextScene();
+        }
+    }
+    */
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
