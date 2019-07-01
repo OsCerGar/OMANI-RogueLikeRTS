a9435208b779215c918e40d8d3d8298246be6df8
CrisMarCab <crisw8mc@gmail.com>
2019-07-01 19:42:43 +0200

    animations meca advanced

--- OMANI-v2.0/Assets/CharacterMovement.cs
+++ OMANI-v2.0/Assets/CharacterMovement.cs
@@ -27,6 +27,8 @@ public class CharacterMovement : MonoBehaviour
     //Animator 
     [SerializeField]
     Animator anim;
+
+    float x, y;
     // Use this for initialization
     void Start()
     {
@@ -39,95 +41,187 @@ public class CharacterMovement : MonoBehaviour
     {
         //This function controls the movement.
         MovementController();
+
+
     }
 
     //Function in charge of the Main Character movement. Sends commands to the animator and allows the character to rotate.
     void MovementController()
     {
+        if (inputs.Laser)
+        {
+            anim.SetBool("Laser", true);
+        }
+        else
+        {
+            anim.SetBool("Laser", false);
+        }
         // If the axis has any sort of input on WASD.
         if (inputs.MovementAxis.x != 0f || inputs.MovementAxis.y != 0f)
         {
-
             onNoMovementTime = 0;
+            anim.SetBool("OnMovement", true);
 
-            //if (controller.isGrounded)
-            //{
-            onMovement = true;
-
-            desiredDirection = new Vector3(inputs.MovementAxis.x, 0f, inputs.MovementAxis.y);
-            //In case the player moves diagonally, it's normalized so that the speed is the same.
-            if (desiredDirection.magnitude > 1)
+            if (inputs.Laser)
             {
-                desiredDirection = desiredDirection.normalized;
+                //if (controller.isGrounded)
+                //{
+                onMovement = true;
+
+
+                desiredDirection = new Vector3(inputs.MovementAxis.x, 0f, inputs.MovementAxis.y);
+                //In case the player moves diagonally, it's normalized so that the speed is the same.
+                if (desiredDirection.magnitude > 1)
+                {
+                    desiredDirection = desiredDirection.normalized;
+                }
+                //}
+
+                desiredDirection.y -= 1 * Time.deltaTime;
+
+
+                float angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+                Vector3 finalDirection = Quaternion.Euler(0, -angle, 0) * desiredDirection;
+
+                x = Mathf.Lerp(x, finalDirection.x, 0.2f);
+                y = Mathf.Lerp(y, finalDirection.z, 0.2f);
+
+                anim.SetFloat("X", x);
+                anim.SetFloat("Y", y);
+
+                Rotate((LookDirection.miradaposition - transform.position).normalized);
             }
-            //}
 
-            desiredDirection.y -= 1 * Time.deltaTime;
+            else
+            {
 
-            //controller.Move(desiredDirection * speed * Time.deltaTime);
+                //if (controller.isGrounded)
+                //{
+                onMovement = true;
 
-            //float angle = Vector3.Angle(Vector3.forward, transform.forward);
-            float angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
-            Vector3 finalDirection = Quaternion.Euler(0, -angle, 0) * desiredDirection;
+                desiredDirection = new Vector3(inputs.MovementAxis.x, 0f, inputs.MovementAxis.y);
+                //In case the player moves diagonally, it's normalized so that the speed is the same.
+                if (desiredDirection.magnitude > 1)
+                {
+                    desiredDirection = desiredDirection.normalized;
+                }
+                //}
+
+                desiredDirection.y -= 1 * Time.deltaTime;
 
 
-            anim.SetFloat("X", finalDirection.x);
-            anim.SetFloat("Y", finalDirection.z);
+                float angle = Vector3.SignedAngle(Vector3.forward, transform.forward, Vector3.up);
+                Vector3 finalDirection = Quaternion.Euler(0, -angle, 0) * desiredDirection;
+
+                float angleDesiredDirection = Vector3.SignedAngle(desiredDirection, transform.forward, Vector3.up);
+
+                if (angleDesiredDirection > 80 && angleDesiredDirection < 150)
+                {
+                    anim.SetBool("TurnLeft", true);
+                }
+                else if (angleDesiredDirection > 150)
+                {
+                    anim.SetBool("TurnLeft180", true);
+                }
+                else if (angleDesiredDirection < -80 && angleDesiredDirection > -150)
+                {
+                    anim.SetBool("TurnRight", true);
+                }
+                else if (angleDesiredDirection < -150)
+                {
+                    anim.SetBool("TurnRight180", true);
+                }
+
+                else
+                {
+                    anim.SetBool("TurnRight", false);
+                    anim.SetBool("TurnRight180", false);
+                    anim.SetBool("TurnLeft", false);
+                    anim.SetBool("TurnLeft180", false);
+                    x = Mathf.Lerp(x, finalDirection.x, 0.2f);
+                    y = Mathf.Lerp(y, finalDirection.z, 0.2f);
+                    anim.SetFloat("X", x);
+                    anim.SetFloat("Y", y);
+
+                }
 
 
-            //Rotate(desiredDirection);
-            Rotate((LookDirection.miradaposition - transform.position).normalized);
 
+
+            }
         }
 
         // If the axis has any sort of input on Joystick.
         else if (inputs.MovementAxisController.x > 0.2f || inputs.MovementAxisController.x < -0.2f || inputs.MovementAxisController.y > 0.2f || inputs.MovementAxisController.y < -0.2f)
         {
             onNoMovementTime = 0;
+            anim.SetBool("OnMovement", true);
 
-
-            //if (controller.isGrounded)
-            //{
-            onMovement = true;
-
-            desiredDirection = new Vector3(inputs.MovementAxisController.x, 0f, inputs.MovementAxisController.y);
-
-            //In case the player moves diagonally, it's normalized so that the speed is the same.
-            if (desiredDirection.magnitude > 1)
+            if (inputs.Laser)
             {
-                desiredDirection = desiredDirection.normalized;
+
+
+                //if (controller.isGrounded)
+                //{
+                onMovement = true;
+
+                desiredDirection = new Vector3(inputs.MovementAxisController.x, 0f, inputs.MovementAxisController.y);
+
+                //In case the player moves diagonally, it's normalized so that the speed is the same.
+                if (desiredDirection.magnitude > 1)
+                {
+                    desiredDirection = desiredDirection.normalized;
+                }
+
+                //}
+                desiredDirection.y -= 1 * Time.deltaTime;
+
+                //controller.Move(desiredDirection * speed * Time.deltaTime);
+                anim.SetFloat("X", desiredDirection.x);
+                anim.SetFloat("Y", desiredDirection.z);
+                LookDirection.LookAtWhileMoving(inputs.MovementAxisController.x, inputs.MovementAxisController.y);
+                Rotate(desiredDirection);
             }
-
-            //}
-            desiredDirection.y -= 1 * Time.deltaTime;
-
-            //controller.Move(desiredDirection * speed * Time.deltaTime);
-            anim.SetFloat("X", desiredDirection.x);
-            anim.SetFloat("Y", desiredDirection.z);
-            LookDirection.LookAtWhileMoving(inputs.MovementAxisController.x, inputs.MovementAxisController.y);
-            Rotate(desiredDirection);
-
         }
 
         else
         {
             desiredDirection = new Vector3(0, 0, 0);
             //controller.Move(desiredDirection * speed);
+
+            anim.SetBool("OnMovement", false);
         }
 
         if (onMovement)
         {
             if (inputs.MovementAxis.x == 0f && inputs.MovementAxis.y == 0f)
             {
+                anim.SetFloat("X", 0);
+                anim.SetFloat("Y", 0);
+
+                anim.SetBool("TurnRight", false);
+                anim.SetBool("TurnRight180", false);
+                anim.SetBool("TurnLeft", false);
+                anim.SetBool("TurnLeft180", false);
+
                 onMovement = false;
+
                 //sound 
                 //OnStopping();
                 onMovementTime = 0;
             }
+
+            anim.SetBool("Iddle", false);
+
         }
         else
         {
             onNoMovementTime += Time.deltaTime;
+
+            if (onNoMovementTime > 5)
+            {
+                anim.SetBool("Iddle", true);
+            }
         }
     }
 
